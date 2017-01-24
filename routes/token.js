ae2f9acb0fbf34f9b47c882ad9266925820764a3
e <goldmouse67>
2017-01-23 22:14:48 -0500

    Working on login

--- routes/token.js
+++ routes/token.js
@@ -18,8 +18,13 @@ exports.getRequestToken = function (req, res)  {
 exports.getAccessToken = function (req, res)  {
    var token = req.query.oauth_token,
 			verifier = req.query.oauth_verifier;
-         twitter.accessToken(token, verifier).then(function(accessToken) {
-            return twitter.verifyCredentials(accessToken);
+         twitter.accessToken(token, verifier).then(function(accessToken,accessSecret) {
+             twitter.verifyCredentials(accessToken, accessSecret, function(err, user) {
+                    if (err)
+                        res.status(500).send(err);
+                    else
+                        res.send(user);
+                });
         }).then(function(user) {
             res.send(user);
         }).catch(function(err) {
